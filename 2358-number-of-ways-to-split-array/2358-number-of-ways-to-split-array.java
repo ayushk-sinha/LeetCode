5270fa5e78b604d30108affa6bdae3e9114f28d3
Ayush k <85569201+ayushk-sinha@users.noreply.github.com>
2025-01-03 21:22:38 +0530

    Time: 3 ms (59.69%), Space: 59.9 MB (35.33%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
@@ -7,11 +7,14 @@ class Solution {
             prefixSum[i] = prefixSum[i-1]+nums[i];
         }
         for(int  i = 0; i < nums.length-1; i++){
-           long leftsum = prefixSum[i];
-           long rightsum = prefixSum[nums.length-1] - leftsum;
+        //    long leftsum = prefixSum[i];
+        //    long rightsum = prefixSum[nums.length-1] - leftsum;
            
-           if(leftsum>=rightsum)
-           ans++;
+        //    if(leftsum>=rightsum)
+        //    ans++;
+        if(prefixSum[i]>=prefixSum[nums.length-1]-prefixSum[i])
+        ans++;
+
         }
         return ans;
     }
